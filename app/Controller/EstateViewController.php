c56eca5f96dd44b2e2a28b77a97bc8aef0e24abf
massongit <13t4038a@vc.ibaraki.ac.jp>
2015-11-26 22:31:40 +0900

    トップ画面を実装

--- app/Controller/EstateViewController.php
+++ app/Controller/EstateViewController.php
@@ -29,21 +29,35 @@ class EstateViewController extends AppController
 	);
 
 	/**
-	 * 物件検索・絞り込み
+	 * 物件検索・絞込・トップ画面
 	 */
 	public function estateList()
 	{
-		if($this->request->is("post")){
+		if ($this->request->is("post")) { //物件検索・絞り込み
 			$this->set("title_for_layout", "物件検索結果");
 			$options=array();
-		}else {
+		} else { //トップ画面
 			$this->set("title_for_layout", "オススメ物件一覧");
-			$options=array(/*"order"=>array("EstateMainFacillitiesDistance.distance")*/);
+			$options = array(
+				"joins" => array(
+					array(
+						"table" => "estate_main_facilities_distances",
+						"alias" => "EstateMainFacilitiesDistance",
+						"conditions" => array(
+							"Estate.estate_id = EstateMainFacilitiesDistance.estate_id",
+							"EstateMainFacilitiesDistance.estate_main_facilities_id=1"
+						)
+					)
+				),
+				"order" => array("EstateMainFacilitiesDistance.distance")
+			);
 		}
-		$this->Estate->virtualFields = array('age' => "strftime('%Y',datetime(strftime('%s',datetime('now','localtime'))-age/1000,'unixepoch'))-1970");
-		$this->Estate->hasMany['EstatePicture']['conditions'] = 'EstatePicture.thumbnail_flag=1';
+		$this->Estate->virtualFields = array("age" => "strftime('%Y',datetime(strftime('%s',datetime('now','localtime'))-Estate.age/1000,'unixepoch'))-1970");
+		$this->Estate->hasMany["EstatePicture"]["conditions"] = "EstatePicture.thumbnail_flag=1";
 		$this->set("estates", $this->Estate->find("all",$options));
-		$this->Estate->hasMany['EstatePicture']['conditions'] = null;
+		foreach ($this->Estate->hasMany as $k => $v) {
+			$this->Estate->hasMany[$k]["conditions"] = "";
+		}
 	}
 
 	/**
--- app/View/EstateView/estate_list.ctp
+++ app/View/EstateView/estate_list.ctp
@@ -1,4 +1,3 @@
-<?php var_dump($estates) ?>
 <table>
 	<thead>
 	<?php echo $this->Html->tableHeaders(array("物件画像", "住所", "家賃", "間取り<br>面積", "窓の向き", "築年数", "不動産業者名")) . PHP_EOL ?>
