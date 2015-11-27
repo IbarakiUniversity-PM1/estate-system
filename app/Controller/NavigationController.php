c6c7298392f11aabd6199b0ec525fe79dfa5b02c
massongit <13t4038a@vc.ibaraki.ac.jp>
2015-11-27 17:27:12 +0900

    検索条件指定・提供不動産業者に、提供不動産業者が表示されるように実装

--- app/Controller/NavigationController.php
+++ app/Controller/NavigationController.php
@@ -6,10 +6,19 @@
 class NavigationController extends AppController
 {
 	/**
+	 * @var array 扱うモデルのリスト
+	 */
+	public $uses = array("EstateAgent");
+
+	/**
 	 * 検索条件指定・提供不動産業者
 	 */
 	public function nav()
 	{
+		//呼び出すテーブルを不動産業者テーブルに限定
+		$this->EstateAgent->recursive = -1;
+		//不動産業者テーブルの内容を取得し、$estate_agentsにセット
+		$this->set("estate_agents", $this->EstateAgent->find("all"));
 		//検索条件指定・提供不動産業者用エレメントを呼び出す
 		$this->render('../Elements/nav');
 	}
--- app/View/Elements/nav.ctp
+++ app/View/Elements/nav.ctp
@@ -1,3 +1,14 @@
-<div>
+<div id="nav">
 	<h4>こうがく不動産</h4>
+	<table>
+		<thead>
+		<?php echo $this->Html->tableHeaders(array("提供不動産業者")) . PHP_EOL ?>
+		</thead>
+		<tbody>
+		<?php foreach ($estate_agents as $e) {
+			$e = $e["EstateAgent"];
+			echo "		" . $this->Html->tableCells(array($e["name"] . "<br>" . $e["phone_number"])) . PHP_EOL;
+		} ?>
+		</tbody>
+	</table>
 </div>
