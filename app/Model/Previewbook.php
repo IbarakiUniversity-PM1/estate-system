adf769748dffbc76acc2b9f4bc17090c4d57a5b4
massongit <13t4038a@vc.ibaraki.ac.jp>
2015-12-01 23:45:06 +0900

    リファクタリング 不要なファイルを削除

--- app/Model/Previewbook.php
+++ app/Model/Previewbook.php
@@ -13,31 +13,31 @@ class PreviewBook extends AppModel
 	 * @var array 内見予約で扱う情報の定義
 	 */
 	public $_schema = array(
-			'user_name' => array(
-				'type' => 'text',
-				'length' => 20
-			),
-			'email_address' => array(
-				'type' => 'text',
-				'length' => 256
-			),
-			'tel_number' => array(
-				'type' => 'integer',
-				'length' => 11
-			),
-			'preview_date_1' => array(
-				'type' => 'date',
-			),
-			'preview_date_2' => array(
-				'type' => 'date',
-			),
-			'preview_date_3' => array(
-				'type' => 'date',
-			),
-			'estate_id' => array(
-				'type' => 'integer'
-			)
-		);
+		'user_name' => array(
+			'type' => 'text',
+			'length' => 20
+		),
+		'email_address' => array(
+			'type' => 'text',
+			'length' => 256
+		),
+		'tel_number' => array(
+			'type' => 'integer',
+			'length' => 11
+		),
+		'preview_date_1' => array(
+			'type' => 'date',
+		),
+		'preview_date_2' => array(
+			'type' => 'date',
+		),
+		'preview_date_3' => array(
+			'type' => 'date',
+		),
+		'estate_id' => array(
+			'type' => 'integer'
+		)
+	);
 	/**
 	 * @var array 入力チェックの定義
 	 */
