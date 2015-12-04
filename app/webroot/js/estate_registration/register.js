8bfd9c757dea78b1c91a1d8c99e3f5185e5f69f2
massongit <13t4038a@vc.ibaraki.ac.jp>
2015-12-04 17:52:41 +0900

    物件画像を複数登録できるように実装

--- app/webroot/js/estate_registration/register.js
+++ app/webroot/js/estate_registration/register.js
@@ -1,7 +1,7 @@
 jQuery(
 	function () {
 		//駐車場フラグを変更したときの挙動
-		function estate_parking_flag_function() {
+		function change_estate_parking_flag() {
 			if ($('#EstateParkingFlag').is(':checked')) { //駐車場フラグが立っているとき
 				//駐車場料金の入力フォームを表示
 				$('#EstateParkingFeeDiv').show();
@@ -15,10 +15,10 @@ jQuery(
 			}
 		}
 		//駐車場フラグを変更したときの挙動をセット
-		$('#EstateParkingFlag').click(estate_parking_flag_function);
+		$('#EstateParkingFlag').click(change_estate_parking_flag);
 
 		//駐車場フラグを変更したときの挙動を実行
-		estate_parking_flag_function();
+		change_estate_parking_flag();
 
 		//物件画像のidとnameにナンバリングを行う
 		function numbering_estate_pictures() {
@@ -33,6 +33,7 @@ jQuery(
 				});
 				$i++;
 			});
+
 			//2行以上あるならば、idとnameのナンバリングを行う
 			if (1 < $i) {
 				$('#estate_pictures').find('tbody tr').each(function (i, e) {
@@ -46,8 +47,7 @@ jQuery(
 			}
 		}
 
-		// アップロードするファイルを選択したときの挙動をセット
-		$('input[type=file]').change(function (e) {
+		function change_estate_picuture(e) {
 			var file = $(e.target).prop('files')[0];
 
 			if (file.type.match('image.*')) { //画像であるとき
@@ -59,15 +59,20 @@ jQuery(
 					}));
 				};
 				reader.readAsDataURL(file);
-				//対象が物件画像であるとき、行を増やし、idとnameのナンバリングを行う
-				if ($(e.target.parentNode.parentNode.parentNode.parentNode).is('#estate_pictures')) {
-					$(e.target.parentNode.parentNode.parentNode).append($('.model tbody').html());
+				//対象が物件画像であり、一番下の行であるならば、行を増やし、idとnameのナンバリングを行う
+				if ($(e.target.parentNode.parentNode.parentNode.parentNode).is('#estate_pictures') && $(e.target.parentNode.parentNode.parentNode.parentNode).find('tbody tr').length == $(e.target.parentNode.parentNode.parentNode.parentNode).find('tbody tr').index(e.target.parentNode.parentNode) + 1) {
+					var s = $('.model tr').clone();
+					$(s).change(change_estate_picuture);
+					$(e.target.parentNode.parentNode.parentNode).append(s);
 					numbering_estate_pictures();
 				}
 			} else { //画像でないとき
 				$(e.target.parentNode.parentNode).find('.previews').html('<div class=\'error-message\'>対応していないファイル形式です</div>');
 			}
-		});
+		}
+
+		// アップロードするファイルを選択したときの挙動をセット
+		$('input[type=file]').change(change_estate_picuture);
 
 		//登録ボタンを押下したときの挙動をセット
 		$('#confirm').click(function () {
