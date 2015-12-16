88d134ae94c572a4608755b65845253f0460f712
massongit <13t4038a@vc.ibaraki.ac.jp>
2015-12-17 00:07:12 +0900

    キーワード検索において、何も入力されていない場合には、エラーメッセージが表示され、検索できないように修正

--- app/webroot/js/elements/nav.js
+++ app/webroot/js/elements/nav.js
@@ -2,7 +2,7 @@ $(function(){
 	var $nav=$('#nav');
 	$nav.find('input[type=checkbox]').removeAttr('name').change(function(e){
 		if($(e.target).attr('checked')){
-			$('#nav').find('.error').hide();
+			$('#nav').find('#search1 .error').hide();
 			$(e.target).parent().parent().find('input[type=number],select').removeAttr('disabled').attr('required',true);
 		}else {
 			$(e.target).parent().parent().find('input,select').each(function(){
@@ -14,10 +14,28 @@ $(function(){
 	});
 	$nav.find('#search1 form').submit(function (e) {
 		if($(e.target).find('input:checked').length==0){
-			$('#nav').find('.error').show();
+			$('#nav').find('#search1 .error').show();
 			return false;
 		}else{
 			return true;
+		}
+	});
+	$nav.find('#search2 form').submit(function (e) {
+		if($(e.target).find('input[type=text]').val()){
+			return true;
+		}else{
+			$('#nav').find('#search2 .error').show();
+			return false;
+		}
+	});
+	$nav.find('#search2 input[type=text]').change(function (e) {
+		var $nav=$('#nav');
+		if($(e.target).val()){
+			$nav.find('#search2 .error').hide();
+			$nav.find('#search2 input[type=submit]').removeAttr('disabled');
+		}else{
+			$nav.find('#search2 .error').show();
+			$nav.find('#search2 input[type=submit]').attr('disabled',true);
 		}
 	});
 	$('#EstateEstateKeywordType').removeAttr('name').change(function(e){
