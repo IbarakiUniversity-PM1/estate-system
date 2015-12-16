63c2cbb24fff777aa3db656b8b2d19b2467945fa
massongit <13t4038a@vc.ibaraki.ac.jp>
2015-12-16 23:52:43 +0900

    条件検索において、何もチェックされていない場合には、検索ボタンを押下するとエラーメッセージが出て、遷移されないように修正

--- app/webroot/js/elements/nav.js
+++ app/webroot/js/elements/nav.js
@@ -1,6 +1,8 @@
 $(function(){
-	$('#nav').find('input[type=checkbox]').removeAttr('name').change(function(e){
+	var $nav=$('#nav');
+	$nav.find('input[type=checkbox]').removeAttr('name').change(function(e){
 		if($(e.target).attr('checked')){
+			$('#nav').find('.error').hide();
 			$(e.target).parent().parent().find('input[type=number],select').removeAttr('disabled').attr('required',true);
 		}else {
 			$(e.target).parent().parent().find('input,select').each(function(){
@@ -8,6 +10,14 @@ $(function(){
 					$(this).attr('disabled',true).removeAttr('required');
 				}
 			});
+		}
+	});
+	$nav.find('#search1 form').submit(function (e) {
+		if($(e.target).find('input:checked').length==0){
+			$('#nav').find('.error').show();
+			return false;
+		}else{
+			return true;
 		}
 	});
 	$('#EstateEstateKeywordType').removeAttr('name').change(function(e){
